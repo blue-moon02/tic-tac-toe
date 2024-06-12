8deeb03afdd7a4bd88ef0e35c71e3f89d9a2a91b
blue-moon02 <chouhan1302@gmail.com>
2024-06-13 01:13:26 +0530

    Updates

---
+++ precache-manifest.e51b2cdb95ec7e0a6d2397bce264a118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270cc53f82c6a5ed0e88e76761849bee",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5b077730abb2ef0942bd",
+    "url": "/tic-tac-toe/static/css/main.c2456740.chunk.css"
+  },
+  {
+    "revision": "d65d3a37338bcaf15f0e",
+    "url": "/tic-tac-toe/static/js/2.c0c1355c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tic-tac-toe/static/js/2.c0c1355c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b077730abb2ef0942bd",
+    "url": "/tic-tac-toe/static/js/main.c1778e07.chunk.js"
+  },
+  {
+    "revision": "f12a80e21270aa522884",
+    "url": "/tic-tac-toe/static/js/runtime-main.cdf35a4e.js"
+  }
+]);
